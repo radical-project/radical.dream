b2c0a3d64ccb05763f7db5f00273828157c6ee55
AymenFJA <aymen.alsaadi@rutgers.edu>
2022-05-27 15:59:51 -0400

    add boto3 as a required pkg

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup_args['name']                 = "hydraa"
 setup_args['version']              = "1.0.0"
 setup_args['packages']             = find_packages()
 setup_args['python_requires']      = '>=3.6'
+setup_args['install_requires']     = ['boto3']
 setup_args['include_package_data'] = True
 
 setup(**setup_args)
