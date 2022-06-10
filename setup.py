df0ec5d0b3a2ab7dc845d2134f961048e87d7b8b
AymenFJA <aymen.alsaadi@rutgers.edu>
2022-06-10 09:42:14 -0400

    add azure as a required pkg

--- setup.py
+++ setup.py
@@ -6,7 +6,14 @@ setup_args['name']                 = "hydraa"
 setup_args['version']              = "1.0.0"
 setup_args['packages']             = find_packages()
 setup_args['python_requires']      = '>=3.6'
-setup_args['install_requires']     = ['boto3']
-setup_args['include_package_data'] = True
+setup_args['install_requires']     = ['boto3',
+                                      'azure-core',
+                                      'azure-batch', 
+                                      'azure-common',
+                                      'azure-identity',
+                                      'azure-mgmt-core',
+                                      'azure-mgmt-storage',
+                                      'azure-mgmt-resource',
+                                      'azure-mgmt-containerinstance']
 
 setup(**setup_args)
