3925eccd53ef989530cc868bab8018164d382827
AymenFJA <aymen.alsaadi@rutgers.edu>
2023-04-03 18:34:59 -0400

    refine

--- hydraa/services/hpc_manager/radical_pilot.py
+++ hydraa/services/hpc_manager/radical_pilot.py
@@ -1,3 +1,4 @@
+import os
 import radical.pilot as rp
 import radical.utils as ru
 
@@ -6,20 +7,32 @@ class RadicalPilot():
 	def __init__(self, pdesc: rp.PilotDescription) -> None:
 
 		self.pdesc = pdesc
+		self.check_pre_start()
+
+		self.session = rp.Session()
+		self.pmgr   = rp.PilotManager(session=self.session)
+		self.tmgr   = rp.TaskManager(session=self.session)
+		pilot = self.pmgr.submit_pilots(self.pdesc)
+
+		# Register the pilot in a TaskManager object.
+		self.tmgr.add_pilots(pilot)
 
 
-	def start(self, tasks):
+	def check_pre_start(self):
+
+		try:
+			os.environ["RADICAL_PILOT_DBURL"]
+		except KeyError:
+			print("Please set RADICAL_PILOT_DBURL?")
+		
+		os.environ.get("RADICAL_PROFILE", "True")
+		os.environ.get("RADICAL_LOG_LVL", "DEBUG")
+
+
+	def submit(self, tasks):
         # Create a new session. No need to try/except this: if session creation
         # fails, there is not much we can do anyways...
 		try:
-			session = rp.Session()
-			pmgr   = rp.PilotManager(session=session)
-			tmgr   = rp.TaskManager(session=session)
-			pilot = pmgr.submit_pilots(self.pdesc)
-
-			# Register the pilot in a TaskManager object.
-			tmgr.add_pilots(pilot)
-
 			tds = list()
 
 			for task in range(tasks):
@@ -34,8 +47,8 @@ class RadicalPilot():
 			# Submit the previously created task descriptions to the
 			# PilotManager. This will trigger the selected scheduler to start
 			# assigning tasks to the pilots.
-			tmgr.submit_tasks(tds)
-			tmgr.wait_tasks()
+			self.tmgr.submit_tasks(tds)
+			self.tmgr.wait_tasks()
 
 		except Exception as e:
 			ru.print_exception_trace()
@@ -45,4 +58,4 @@ class RadicalPilot():
 			ru.print_exception_trace()
 		
 		finally:
-			session.close(download=True)
+			self.session.close(download=True)
