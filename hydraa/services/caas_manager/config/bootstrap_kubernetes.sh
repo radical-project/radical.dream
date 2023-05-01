e32af0c14388a8aab9690f11120f90a2915bcb77
AymenFJA <aymen.alsaadi@rutgers.edu>
2023-04-30 20:09:46 -0400

    ven or virtualenv bootstrap

--- hydraa/services/caas_manager/config/bootstrap_kubernetes.sh
+++ hydraa/services/caas_manager/config/bootstrap_kubernetes.sh
@@ -6,12 +6,35 @@ KUBESPRAYDIR=$(pwd)/kubespray
 VENVDIR="$KUBESPRAYDIR/.venv"
 ANSIBLE_VERSION=2.12
 
-virtualenv  --python=$(which python3) $VENVDIR
+declare -a PIP=$(which pip3)
+declare -a PYTHON=$(which python3)
+
+# support both venv and virtualenv
+if virtualenv --python=$PYTHON $VENVDIR; then
+        echo "$VENVDIR is created with virtualenv"
+else
+    echo "failed to create $VENVDIR with virtualenv"
+
+    # try to create venv
+    if $(which python3) -m venv $VENVDIR; then
+       echo "$VENVDIR is created with venv"
+    else
+       echo "failed to create $VENVDIR with venv, exiting"
+       exit 1
+    fi
+fi
 
 source $VENVDIR/bin/activate
 cd $KUBESPRAYDIR
 
-pip install -U -r requirements-$ANSIBLE_VERSION.txt
+declare -a PIP=$(which pip3)
+declare -a PYTHON=$(which python3)
+
+# https://stackoverflow.com/q/34819221/5977059
+$PIP install wheel
+$(which python3) setup.py bdist_wheel
+
+$PIP install -U -r requirements-$ANSIBLE_VERSION.txt
 
 # copy the sample inventory definitions from the repo.
 cp -rfp inventory/sample inventory/mycluster
@@ -30,11 +53,11 @@ export KUBE_CONTROL_HOSTS=1
 
 # set the nodes names and ips (from controller to workers)
 declare -a MAP=($map)
-CONFIG_FILE=inventory/mycluster/hosts.yml python3 contrib/inventory_builder/inventory.py ${MAP[@]}
+CONFIG_FILE=inventory/mycluster/hosts.yml $PYTHON contrib/inventory_builder/inventory.py ${MAP[@]}
 
 sed -i "s/\boverride_system_hostname: true\b/override_system_hostname: false/g" "roles/bootstrap-os/defaults/main.yml"
 
-# invoke the ansible playbook
+# start the ansible playbook
 ansible-playbook -i inventory/mycluster/hosts.yml --private-key=$key -u $user --become cluster.yml  1>> ansible.out 2>> ansible.err
 
 # setup the master node kube config
